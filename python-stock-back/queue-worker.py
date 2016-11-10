2dd0bd7e1ed36873b9533fa03a06f24ba6121bd1
neilpeterson <neilpeterson@outlook.com>
2016-11-09 21:29:57 -0800

    added service logic

--- python-stock-back/queue-worker.py
+++ python-stock-back/queue-worker.py
@@ -1,15 +1,19 @@
 from azure.storage.queue import QueueService
 import json
+import math
 import os
 import random
 import requests
+import time
 
-# grab environment variables
+## CURENT NOTES - Things are not efficient, workers are stopping before queue cleared, which starts up more, etc. etc. Process works but not well.
+
+# Grab environment variables.
 azurestoracct = os.environ['azurestoracct']
 azurequeue = os.environ['azurequeue']
 azurequeuekey = os.environ['azurequeuekey'] + "==;"
 image = os.environ['image']
-queuelength = os.environ['queuelength']
+queue_length = os.environ['queuelength']
 
 if "docker" in os.environ:
     docker = os.environ['docker']
@@ -19,52 +23,82 @@ if "chronos" in os.environ:
 
 while True:
 
-    # set up azure queue
+    print("Top")
+    time.sleep(1)
+
+    # Set up azure queue / get count of messages on queue (count).
     queue_service = QueueService(account_name=azurestoracct, account_key=azurequeuekey)
     metadata = queue_service.get_queue_metadata(azurequeue)
     count = metadata.approximate_message_count
-    print(count)    
             
     if "docker" in os.environ:
 
-        # get report container count
-
-        print(docker)
-        
-        #reportcount = <docker query>
+        # Get containers from Docker API.
         headers = {'Content-Type': 'application/json'}
-        r2 = requests.get(docker + "json?all=1&before=8dfafdbc3a40&size=1")
-        print(r2.Content)
+        containers = json.loads((requests.get(docker + "json?all=1&before=8dfafdbc3a40&size=1")).text)
 
-        #if count > reportcount * queuelength
+        # Initialize count ticker
+        containers_count = 0
 
-        # docker container header
-        #data = json.loads('{"Image": "' + image + '"]}')
-        #print(data)
-    
-        # create and start docker container
-        #headers = {'Content-Type': 'application/json'}
-        #r = requests.post(docker + "create", data=json.dumps(data), headers=headers)
-        #b = json.loads(r.text)
-        #x = requests.post(docker + b['Id'] + "/start")
+        # Determine how many containers match worker / set number for loop.
+        for container in containers:
+            if container['Image'] == image:
+                containers_count += 1
 
-    if "chronos" in os.environ:
+        # Determine how many workers are required
+        needed_workers = math.ceil(count/int(queue_length))  
+        start_workers_count = math.ceil(needed_workers - containers_count)
 
-        # docker container header
-        randomint = random.randint(1, 100000)            
-        data = json.loads('{"schedule": "R0/2016-09-28T22:55:00Z/PT24H","name":"' + str(randomint) + '","cpus": "0.1","mem": "32","command": "docker run ' + image + '"}')
-        print(data)
+        print("Items on Queue: " + str(count) + " --- Current Workers: " + str(containers_count) + " --- Needed Workers: " + str(needed_workers) + " --- To Start: " + str(start_workers_count))
 
-        # create and start docker container
-        headers = {'Content-Type': 'application/json'}
-        r = requests.post(chronos + "scheduler/iso8601", data=json.dumps(data), headers=headers)
-        print (r)
-        x = requests.put(chronos + 'scheduler/job/' + str(randomint))
+        #### STUCK RIGHT HERE - OPERATOR IS NOT WORKING
+        # Start additional workers if needed
+        if start_workers_count >= needed_workers and start_workers_count > 0:
 
-    #else
+            print("Need to Deploy Workers")
 
-        # remove report containers
+            # Loop through worker deployment.
+            #while start_workers_count >= needed_workers:
+            while needed_workers >= start_workers_count and needed_workers > 0:
 
-        #queue = 4, container = 2 = no change
-        #queue = 4, containers = 1 = add 1
-        #queue = 10, containers = 2 = add 4
+                i = needed_workers
+                needed_workers = 0
+                while i > 0:
+
+                    print("Starting worker container")
+
+                    # Start container .via Docker REST API.
+                    headers = {'Content-Type': 'application/json'}
+                    data = json.loads('{"Image": "' + image + '"}')
+                    docker_start_response = json.loads(requests.post(docker + "create", data=json.dumps(data), headers=headers).text)
+                    requests.post(docker + docker_start_response['Id'] + "/start")
+
+                    # Decrement loop counter
+                    i -= 1
+                    print(i)
+
+        else: 
+            if start_workers_count <= needed_workers and start_workers_count < 0:
+
+                print("Need to Remove Workers")
+
+                # Stop and remove workers
+                while start_workers_count < needed_workers:
+
+                    headers = {'Content-Type': 'application/json'}
+                    docker_get_response = json.loads(requests.get(docker + "json?all=1&before=8dfafdbc3a40&size=1").text)
+
+                    for container in docker_get_response:                        
+                        
+                        if container['Image'] == image:
+
+                            print("Removing contianer: " + container['Image'])
+                            
+                            id = container['Id'] 
+                            headers = {'Content-Type': 'application/json'}
+                            requests.post(docker + id + "/stop", headers=headers)
+                            requests.delete(docker + id, headers=headers)      
+
+                            # Increment loop counter
+                            start_workers_count += 1
+
